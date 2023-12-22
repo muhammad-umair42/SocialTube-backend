ccc259e4cee9de32ff3d870e7aece5f025ef4e72
Muhammad Umair <umair42.dev@gmail.com>
2023-12-22 09:44:26 +0500

    Cloudinary

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,25 @@
+import { v2 as cloudinary } from "cloudinary";
+
+//Cloudinary config
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: CLOUDINARY_API_SECRET,
+});
+
+//Saving file
+const uploadOnCloudinary = async function (localFilePath) {
+  try {
+    if (!localFilePath) return null;
+    const result = await cloudinary.v2.uploader.upload(localFilePath, {
+      resource_type: "auto",
+    });
+
+    console.log("File successfully uploaded", result.url);
+  } catch (error) {
+    fs.unlinkSync(localFilePath);
+    return null;
+  }
+};
+
+export { uploadOnCloudinary };
