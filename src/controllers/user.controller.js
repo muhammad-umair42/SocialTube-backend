aee6719d2fea14744315cfbb88d8a5596bac303d
Muhammad Umair <umair42.dev@gmail.com>
2023-12-22 12:53:43 +0500

    Register controller

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,8 +1,20 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
-
+import { ApiError } from "../utils/ApiError.js";
 export const registerUser = asyncHandler(async (req, res) => {
-  res.status(200).json({
-    success: true,
-    message: "User registered successfully",
-  });
+  //get user details from client
+  const { name, email, password, fullName } = req.body;
+
+  //validate user details - not empty
+  if ([name, email, password, fullName].some((field) => field?.trim() === "")) {
+    throw new ApiError(400, "Please fill all fields");
+  }
+
+  //check if user already exists: username, email
+
+  //check for images and avatar
+  //upload them to cloud storage
+  //create user object - entry in database
+  //remove password and refresh token from response
+  //check for user created
+  //return response
 });
